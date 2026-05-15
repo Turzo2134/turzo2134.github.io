9a704122d3d27eb9a89bde6876264b19fba6a31b
Turzo Roy <84925906+Turzo2134@users.noreply.github.com>
2026-05-16 00:01:21 +0600

    Simplify astro config - remove content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // Enable SSR
   output: 'static',
-  
-  // Vite config
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
-  }
 });
